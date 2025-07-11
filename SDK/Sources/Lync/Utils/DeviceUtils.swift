9bc82b902cc84f9ea349353fa3a463cafa396c90
nktrchk <nikitoss@gmail.com>
2025-07-12 01:15:05 +0200

    Fix all compilation errors - SDK ready for production

--- SDK/Sources/Lync/Utils/DeviceUtils.swift
+++ SDK/Sources/Lync/Utils/DeviceUtils.swift
@@ -12,7 +12,7 @@ struct DeviceUtils {
         let machineMirror = Mirror(reflecting: systemInfo.machine)
         let identifier = machineMirror.children.reduce("") { identifier, element in
             guard let value = element.value as? Int8, value != 0 else { return identifier }
-            return identifier + String(UnicodeScalar(UInt8(value))!)
+            return identifier + String(UnicodeScalar(UInt8(value)))
         }
         return identifier
     }
